0fe6611da22c176e1d46b0e9546857e0f7640428
Tsung-Ju Chiang <71379180+tsung-ju@users.noreply.github.com>
2022-09-27 15:34:04 -0400

    Add support for v1

--- src/background.js
+++ src/background.js
@@ -1,14 +1,36 @@
 import { decodeHTML } from "entities";
 
+// See: https://help.proofpoint.com/Threat_Insight_Dashboard/Concepts/How_do_I_decode_a_rewritten_URL%3F
+
+function decodeV1(rewrittenUrl) {
+  let target = new URL(rewrittenUrl).searchParams.get("u");
+  target = decodeURIComponent(target);
+  target = decodeHTML(target);
+  return target;
+}
+
+function decodeV2(rewrittenUrl) {
+  let target = new URL(rewrittenUrl).searchParams.get("u");
+  target = target.replaceAll("-", "%").replaceAll("_", "/");
+  target = decodeURIComponent(target);
+  target = decodeHTML(target);
+  return target;
+}
+
 browser.webRequest.onBeforeRequest.addListener(
-  function redirectProofpointV2(details) {
-    let target = new URL(details.url).searchParams.get("u");
-    target = target.replaceAll("-", "%").replaceAll("_", "/");
-    target = decodeURIComponent(target);
-    target = decodeHTML(target);
-    return {
-      redirectUrl: target,
-    };
+  function (details) {
+    return { redirectUrl: decodeV1(details.url) };
+  },
+  {
+    urls: ["https://urldefense.proofpoint.com/v1/url?*"],
+    types: ["main_frame", "sub_frame"],
+  },
+  ["blocking"]
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    return { redirectUrl: decodeV2(details.url) };
   },
   {
     urls: ["https://urldefense.proofpoint.com/v2/url?*"],
